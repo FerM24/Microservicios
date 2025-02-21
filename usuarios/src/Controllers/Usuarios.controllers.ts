2a4120add7fcaab2c51176d7d32eac0976bcb4db
Fernanda <fernanda.escobar63@unach.mx>
2025-02-20 19:27:39 -0600

    El segundo commit

--- usuarios/src/Controllers/Usuarios.controllers.ts
+++ usuarios/src/Controllers/Usuarios.controllers.ts
@@ -3,4 +3,5 @@ import { Request, Response } from "express";
 export const getAll = (req: Request, res: Response) => {
     // res.send("lista de productos");
     res.status(200).json({ Hola: "Usuarios" });
+    
 };
